67115f62cf2b5c0522be722c990ba2439c0f95d1
Ben Evans <bensqldba@gmail.com>
2013-11-21 22:56:59 -0500

    Update main.c

---
+++ _init/main.c
@@ -0,0 +1,32 @@
+#define ARMBASE 0x8000
+#define CS 0x20003000
+#define CLO 0x20003004
+#define C0 0x2000300C
+#define C1 0x20003010
+#define C2 0x20003014
+#define C3 0x20003018
+
+#define GPFSEL1 0x20200004
+#define GPSET0  0x2020001C
+#define GPCLR0  0x20200028
+
+
+int kernal_main_start ( void )
+{
+  
+  //probably best to setup DMA controller first then build scheduler
+  
+}
+
+
+//-------------------------------------------------------------------------
+//
+// Copyright (c) 2013 Benjamin Evans benevans@zencode.me
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+//
+//-------------------------------------------------------------------------
