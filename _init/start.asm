9a320a455db72f3839c477cf3d8160be870df65b
Ben Evans <bensqldba@gmail.com>
2013-11-28 13:38:43 -0500

    updated startup to work with vector table

--- _init/start.asm
+++ _init/start.asm
@@ -12,51 +12,64 @@
 .section .init
 .global _start
 _start:
-	;@ We start by declaring out interupt vector table
 	ldr pc,reset_handler
-	ldr pc,undefined_handler
-	ldr pc,swi_handler
-	ldr pc,prefetch_handler
-	ldr pc,data_handler
-	ldr pc,unused_handler
-	ldr pc,irq_handler
-	ldr pc,fiq_handler
-	
-reset_handler:      .word reset
-undefined_handler:  .word hang
-swi_handler:        .word hang
-prefetch_handler:   .word hang
-data_handler:       .word hang
-unused_handler:     .word hang
-irq_handler:        .word irq
-fiq_handler:        .word hang
+    ldr pc,undefined_handler
+    ldr pc,swi_handler
+    ldr pc,prefetch_handler
+    ldr pc,data_handler
+    ldr pc,unused_handler
+    ldr pc,irq_handler
+    ldr pc,fiq_handler
+	reset_handler:      .word reset
+	undefined_handler:  .word hang
+	swi_handler:        .word hang
+	prefetch_handler:   .word hang
+	data_handler:       .word hang
+	unused_handler:     .word hang
+	irq_handler:        .word irq
+	fiq_handler:        .word hang
 
+.section .text
 
 ;@ We must first copy the vector table to 0x0000
 ;@ where the PI is expecting it to be
 reset:
 	mov r0,#0x8000
-	mov r1,#0x0000
-	ldmia r0!,{r2,r3,r4,r5,r6,r7,r8,r9}
-	stmia r1!,{r2,r3,r4,r5,r6,r7,r8,r9}
-	ldmia r0!,{r2,r3,r4,r5,r6,r7,r8,r9}
-	stmia r1!,{r2,r3,r4,r5,r6,r7,r8,r9}
-	
+    mov r1,#0x0000
+    ldmia r0!,{r2,r3,r4,r5,r6,r7,r8,r9}
+    stmia r1!,{r2,r3,r4,r5,r6,r7,r8,r9}
+    ldmia r0!,{r2,r3,r4,r5,r6,r7,r8,r9}
+    stmia r1!,{r2,r3,r4,r5,r6,r7,r8,r9}
+
+	mov sp,#0x8000
+
 	
 	bl kernal_init
 	
 hang: b hang
 
-	
+.section .text
+
 .global GET32
 GET32:
 	ldr r0,[r0]
 	bx lr
+
 	
 .global PUT32
 PUT32:
 	str r1,[r0]
 	bx lr
+	
+.globl PUT16
+PUT16:
+    strh r1,[r0]
+    bx lr
+
+.globl PUT8
+PUT8:
+    strb r1,[r0]
+    bx lr
 
 .global TurnOnLight
 TurnOnLight:
