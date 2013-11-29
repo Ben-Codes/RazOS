a88f654927b93797f29176937acc448648a8fcd9
Ben Evans <bensqldba@gmail.com>
2013-11-29 16:38:30 -0500

    removed init

--- _init/framebuffer.h
+++
@@ -1,8 +0,0 @@
-#ifndef FRAMEBUFFER_H_INCLUDED
-#define FRAMEBUFFER_H_INCLUDED
-
-extern unsigned int InitialiseFrameBuffer(unsigned int width,unsigned int height,unsigned int bitDepth);
-extern void WritePixel(unsigned int x, unsigned int y, unsigned short value);
-
-
-#endif
